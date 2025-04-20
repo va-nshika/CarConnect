3c393c9bff0061fa3fe82ea53efd64b2648aff3a
Vanshika <vanshikaukey1@gmail.com>
2025-04-20 21:21:19 +0530

    Update DatabaseContext .java

--- DatabaseContext .java
+++ DatabaseContext .java
@@ -1,19 +1,38 @@
 package util;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseContext {
     private String propertiesFile;
 
+    /**
+     * @param propertiesFile path to db.properties (e.g. "db.properties" or "resources/db.properties")
+     */
     public DatabaseContext(String propertiesFile) {
         this.propertiesFile = propertiesFile;
     }
 
+    /**
+     * Loads JDBC settings from properties file and returns a Connection.
+     */
     public Connection getConnection() throws IOException, SQLException {
-        // Reads "db.url", "db.user", "db.password" from your properties file
-        String connString = DBPropertyUtil.getConnectionString(propertiesFile);
-        return DBConnUtil.getConnection(connString);
+        // 1. Load the properties
+        Properties props = new Properties();
+        try (FileInputStream in = new FileInputStream(propertiesFile)) {
+            props.load(in);
+        }
+
+        // 2. Read individual settings
+        String url      = props.getProperty("db.url");
+        String user     = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        // 3. Return a live JDBC connection
+        return DriverManager.getConnection(url, user, password);
     }
 }
